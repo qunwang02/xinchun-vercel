c5cd2473d2d13562da3b23d5112bddb3e0ec2afe
qunwang02 <51785600+qunwang02@users.noreply.github.com>
2026-09-19 17:08:55 +0800

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,2 +1,7 @@
-const app = require('../server/server');
-module.exports = app;
+try {
+  const app = require('../server/server');
+  module.exports = app;
+} catch (err) {
+  console.error('❌ 加载 server.js 失败:', err.stack);
+  throw err;
+}
